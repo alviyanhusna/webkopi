c932af50f0ef7f46338d030fa671ac7fa61a8e0e
Alviyan Dhafin Husna <alviyan@gin.co.id>
2024-11-23 05:50:20 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// togle class active hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// toggle kela aktif untuk form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+// toggle aktif untuk shopping cart
+
+const shoppingChart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    shoppingChart.classList.toggle('active');
+    e.preventDefault();
+};
+
+// klik di luar element
+
+const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
+const shopChart = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+
+    if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+
+    if (!shopChart.contains(e.target) && !shoppingChart.contains(e.target)) {
+        shoppingChart.classList.remove('active');
+    }
+});
+
+// modal box
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+
+});
+
+
+// clik close detail
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+};
+
+// click do luar
+
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = "none";
+    }
+};
---
+++ src/app.js
@@ -0,0 +1,81 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('products', () => ({
+        items: [
+            { id: 1, name: 'torabika', img: '1.jpeg', price: 20000 },
+            { id: 2, name: 'nescaffe', img: '2.jpg', price: 25000 },
+            { id: 3, name: 'kapal api', img: '3.jpeg', price: 40000 },
+            { id: 4, name: 'good day', img: '4.jpg', price: 30000 },
+            { id: 5, name: 'abc', img: '5.jpg', price: 25000 },
+        ],
+    }));
+
+    Alpine.store('cart', {
+        items: [],
+        total: 0,
+        quantity: 0,
+        add(newItem) {
+            // cek apahah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // jika kosong / belum ada
+            if (!cartItem) {
+                this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+                this.quantity++;
+                this.total += newItem.price;
+            }
+            else {
+                // jika barang sudah ada, cek apakah barang beda atau sama di cart
+                this.items = this.items.map((item) => {
+                    // juka barang berbeda
+                    if (item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id) {
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jika barang lebih dari 1
+            if (cartItem.quantity > 1) {
+                // telusuri satu2
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang di klik
+                    if (item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                });
+            } else if (cartItem.quantity === 1) {
+                // jika barang sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
+        },
+    });
+
+});
+
+
+// konversi rupiah
+
+const rupiah = (number) => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+    }).format(number);
+};
