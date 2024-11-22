4567df8f2c7c9d86f611066a56979a6bb5e0a9f6
Alviyan Dhafin Husna <alviyan@gin.co.id>
2024-11-23 06:13:37 +0700

    remove

--- js/script.js
+++ js/script.js
@@ -45,18 +45,6 @@ document.addEventListener('click', function (e) {
     }
 });
 
-// modal box
-
-const itemDetailModal = document.querySelector('#item-detail-modal');
-const itemDetailButtons = document.querySelectorAll('.item-detail-button');
-
-itemDetailButtons.forEach((btn) => {
-    btn.onclick = (e) => {
-        itemDetailModal.style.display = 'flex';
-        e.preventDefault();
-    };
-
-});
 
 
 // clik close detail
